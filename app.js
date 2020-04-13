0aed5dcc2c6a18d7f0776a202d4895c890d6fc05
sharbel chris <sharbel.cosa@gmail.com>
2020-04-13 15:57:48 +0300

    make server and graphql communicate

---
+++ app.js
@@ -0,0 +1,26 @@
+import { PubSub, GraphQLServer} from "graphql-yoga";
+import { importSchema} from "graphql-import";
+import jwt from 'jsonwebtoken';
+import  mongoose  from 'mongoose';
+import  dotenv from 'dotenv';
+import resolvers from './graphql/resolvers';
+
+const typeDefs = importSchema('graphql/schema/schema.graphql');
+
+dotenv.config();
+
+const pubsub = new PubSub();
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+    context: { pubsub } });
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+mongoose.connection.once("open", () =>
+    server.start({ port: process.env.PORT },() => console.log("Running at localhost:4400"))
+);
---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    }, password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
